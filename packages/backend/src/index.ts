849b1a6fa44993edba996058660d729d56a19d89
recoil <leev126g8@gmail.com>
2024-02-19 21:34:48 +0800

    feat: 🎸 init SQLite

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,8 +1,23 @@
 import express, { Request, Response } from 'express';
+import { verbose } from 'sqlite3';
+import { config } from 'dotenv';
+
+config(); //setting .env
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+const SQLite = verbose();
+const db = new SQLite.Database(process.env.DATABASE_URL!, SQLite.OPEN_READWRITE, handleSqliteError);
+
+function handleSqliteError(err: Error | null) {
+  if (err instanceof Error) {
+    return console.log(err.message);
+  }
+
+  console.log('connect database successfully');
+}
+
 app.use(express.json());
 
 app.listen(port, () => {
