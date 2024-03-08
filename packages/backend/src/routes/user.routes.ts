d4aca6bbc5ae0f0fafe849ef8e44c54bd19d8124
augustine <ashramwen@gmail.com>
2024-03-08 17:22:56 +0800

    feat: Add regiter/login routes

--- packages/backend/src/routes/user.routes.ts
+++ packages/backend/src/routes/user.routes.ts
@@ -1,5 +1,10 @@
 import { Router, Request, Response } from 'express';
 import { PrismaClient } from 'db';
+import * as bcrypt from 'bcryptjs';
+
+function comparePassword(password: string, hash: string) {
+  return bcrypt.compareSync(password, hash);
+}
 
 const router = Router();
 
@@ -9,9 +14,13 @@ router.get('/', (req: Request, res: Response) => {
 
 router.post('/register', async (req: Request, res: Response) => {
   const { email, password } = req.body;
+
   try {
+    const hash = bcrypt.hashSync(password, 10);
+
+    const userData = { email, password: hash, name: '' };
     const prisma = new PrismaClient();
-    const user = await prisma.user.create({ data: { email, password } });
+    const user = await prisma.user.create({ data: userData });
 
     res.sendStatus(201);
   } catch (err) {
@@ -21,11 +30,19 @@ router.post('/register', async (req: Request, res: Response) => {
 
 router.post('/login', async (req: Request, res: Response) => {
   const { email, password } = req.body;
+
   try {
     const prisma = new PrismaClient();
     const user = await prisma.user.findUnique({ where: { email } });
+    const result = comparePassword(password, user?.password ?? '');
 
-    res.json(user);
+    if (user && result) {
+      res.status(200).json({
+        user: { id: user.id, name: user.name, email: user.email },
+      });
+    } else {
+      res.status(400).json({ message: 'Login failed.' });
+    }
   } catch (err) {
     res.json(err);
   }
