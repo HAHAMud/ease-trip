a4516c3ca8ca99d92303a92bf42fead372d5db39
augustine <ashramwen@gmail.com>
2024-03-28 00:55:07 +0800

    feat: Add startAdornment and endAdornment to EzTextField

--- packages/easy-ui/src/EzTextField/EzTextField.tsx
+++ packages/easy-ui/src/EzTextField/EzTextField.tsx
@@ -1,8 +1,13 @@
 import styled from '@emotion/styled';
-import { TextField, TextFieldProps } from '@mui/material';
+import { InputAdornment, TextField, TextFieldProps } from '@mui/material';
+import { ReactNode } from 'react';
 import { Controller, UseControllerProps, useController } from 'react-hook-form';
 
-type EzTextFieldProps = Omit<TextFieldProps, 'name' | 'value' | 'defaultValue'> & UseControllerProps;
+type EzTextFieldProps = Omit<TextFieldProps, 'name' | 'value' | 'defaultValue'> &
+  UseControllerProps & {
+    startAdornment?: ReactNode;
+    endAdornment?: ReactNode;
+  };
 
 const StyledTextField = styled(TextField)<EzTextFieldProps>(() => ({
   variant: 'standard',
@@ -10,7 +15,8 @@ const StyledTextField = styled(TextField)<EzTextFieldProps>(() => ({
 }));
 
 export function EzTextField(props: EzTextFieldProps) {
-  const { control, name, rules, defaultValue, shouldUnregister, ...componentProps } = props;
+  const { control, name, rules, defaultValue, shouldUnregister, startAdornment, endAdornment, ...componentProps } =
+    props;
   const controllerParams = { control, name, rules, defaultValue, shouldUnregister };
   const { fieldState } = useController(controllerParams);
 
@@ -27,6 +33,14 @@ export function EzTextField(props: EzTextFieldProps) {
           variant={variant}
           helperText={fieldState.error?.message ?? ''}
           error={Boolean(fieldState.error)}
+          InputProps={{
+            ...(startAdornment && {
+              startAdornment: <InputAdornment position="start">{startAdornment}</InputAdornment>,
+            }),
+            ...(endAdornment && {
+              endAdornment: <InputAdornment position="end">{endAdornment}</InputAdornment>,
+            }),
+          }}
         />
       )}
     />
--- packages/frontend/src/modules/Auth/views/Login.tsx
+++ packages/frontend/src/modules/Auth/views/Login.tsx
@@ -9,7 +9,6 @@ import {
   EzForm,
   EzTextField,
   EzCheckbox,
-  InputAdornment,
   IconButton,
 } from '@ease-trip/easy-ui';
 import { Visibility, VisibilityOff } from '@ease-trip/easy-ui/icon';
@@ -18,67 +17,70 @@ import type { LoginForm } from '../models';
 import { useState } from 'react';
 
 export default function LoginPage() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const [showPassword, setShowPassword] = useState(false);
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-      event.preventDefault();
-    };
-  
-    async function handleSubmit(data: LoginForm) {
-      const { email, password } = data;
-  
-      try {
-        const response = await fetch('/api/auth/login', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ email, password }),
-        });
-  
-        const payload = await response.json();
-        if (response.ok) {
-          router.push('/plan');
-        } else {
-          // Handle errors
-          throw Error(payload);
-        }
-      } catch (err) {
-        if (err instanceof Error) {
-        }
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  async function handleSubmit(data: LoginForm) {
+    const { email, password } = data;
+
+    try {
+      const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const payload = await response.json();
+      if (response.ok) {
+        router.push('/plan');
+      } else {
+        // Handle errors
+        throw Error(payload);
+      }
+    } catch (err) {
+      if (err instanceof Error) {
       }
     }
-  
-    return (
-      <Dialog open>
-        <DialogTitle>Ease Trip</DialogTitle>
-        <DialogContent>
-          <Grid rowSpacing={2}>
-            <EzForm defaultValues={defaultValues} schema={loginSchema} onSubmit={handleSubmit}>
-              <EzTextField fullWidth name="email" label="Email" />
-              <EzTextField fullWidth name="password" label="Password" InputProps={{
-                type: showPassword ? 'text' : 'password', 
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }} />
-              <EzCheckbox name="rememberMe" label="keep me signed in." />
-              <DialogActions>
-                <Button variant="contained" type="submit">
-                  Sign In
-                </Button>
-              </DialogActions>
-            </EzForm>
-          </Grid>
-        </DialogContent>
-      </Dialog>
-    );
   }
+
+  return (
+    <Dialog open>
+      <DialogTitle>Ease Trip</DialogTitle>
+      <DialogContent>
+        <Grid rowSpacing={2}>
+          <EzForm defaultValues={defaultValues} schema={loginSchema} onSubmit={handleSubmit}>
+            <EzTextField fullWidth name="email" label="Email" />
+            <EzTextField
+              fullWidth
+              name="password"
+              label="Password"
+              type={showPassword ? 'text' : 'password'}
+              endAdornment={
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              }
+            />
+
+            <EzCheckbox name="rememberMe" label="keep me signed in." />
+
+            <DialogActions>
+              <Button variant="contained" type="submit">
+                Sign In
+              </Button>
+            </DialogActions>
+          </EzForm>
+        </Grid>
+      </DialogContent>
+    </Dialog>
+  );
+}
