479af964bcfe085400e20264eddecc09bdbc2c75
willy <willy8742891@gmail.com>
2024-03-27 23:59:26 +0800

    feat: Password Input.

--- packages/frontend/src/modules/Auth/views/Login.tsx
+++ packages/frontend/src/modules/Auth/views/Login.tsx
@@ -9,12 +9,22 @@ import {
   EzForm,
   EzTextField,
   EzCheckbox,
+  InputAdornment,
+  IconButton,
 } from '@ease-trip/easy-ui';
+import { Visibility, VisibilityOff } from '@ease-trip/easy-ui/icon';
 import { defaultValues, loginSchema } from '../models';
 import type { LoginForm } from '../models';
+import { useState } from 'react';
 
 export default function LoginPage() {
     const router = useRouter();
+
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+    };
   
     async function handleSubmit(data: LoginForm) {
       const { email, password } = data;
@@ -46,9 +56,21 @@ export default function LoginPage() {
           <Grid rowSpacing={2}>
             <EzForm defaultValues={defaultValues} schema={loginSchema} onSubmit={handleSubmit}>
               <EzTextField fullWidth name="email" label="Email" />
-              <EzTextField fullWidth name="password" type="password" label="密碼" />
+              <EzTextField fullWidth name="password" label="Password" InputProps={{
+                type: showPassword ? 'text' : 'password', 
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }} />
               <EzCheckbox name="rememberMe" label="keep me signed in." />
-
               <DialogActions>
                 <Button variant="contained" type="submit">
                   Sign In
