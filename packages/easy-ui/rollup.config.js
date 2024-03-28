d7f88cbeaabb1c0cafcb9475e0ad041bb1048508
Augustine <ashramwen@users.noreply.github.com>
2024-03-28 08:36:28 +0800

    chore: Modify rollup config to ignore "use client" (#56)

--- packages/easy-ui/rollup.config.js
+++ packages/easy-ui/rollup.config.js
@@ -23,6 +23,12 @@ const external = [
  */
 module.exports = [
   {
+    onwarn(warning, warn) {
+      if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes(`"use client"`)) {
+        return;
+      }
+      warn(warning);
+    },
     input: {
       index: './src/index.ts',
       next: './src/next.ts',
