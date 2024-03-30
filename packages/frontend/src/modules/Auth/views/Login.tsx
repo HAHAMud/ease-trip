dc94b34714372658ae5a89f6a0970054f5f5d9af
recoil <leev126g8@gmail.com>
2024-03-30 17:09:13 +0800

    feat: 🎸 add snackbar for login

--- packages/frontend/src/modules/Auth/views/Login.tsx
+++ packages/frontend/src/modules/Auth/views/Login.tsx
@@ -1,32 +1,33 @@
-import { MouseEvent as ReactMouseEvent, useState } from 'react';
+import { Fragment, MouseEvent as ReactMouseEvent, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useMutation } from '@tanstack/react-query';
 import {
+  Alert,
   Dialog,
   DialogContent,
   DialogActions,
   DialogTitle,
   Grid,
+  Snackbar,
+  Slide,
   EzForm,
   EzTextField,
   EzCheckbox,
   EzIconButton,
   EzContainedButton,
 } from '@ease-trip/easy-ui';
+
 import { login } from '@/api/auth';
 import { defaultValues, LoginForm, loginSchema } from '../models';
 
 export default function LoginPage() {
   const router = useRouter();
-
+  const [hasError, setHasError] = useState<boolean>(false);
   const mutation = useMutation({
     mutationFn: login,
     onSuccess: (data) => {
       console.log('🚀 ~ LoginPage ~ data:', data);
       // router.push('/plan');
-    },
-    onError(error, variables, context) {
-      console.log('onError', error);
     },
   });
 
@@ -39,45 +40,60 @@ export default function LoginPage() {
   };
 
   const handleSubmit = (data: LoginForm) => {
-    mutation.mutateAsync(data);
+    setHasError(false);
+    mutation.mutateAsync(data).catch(() => {
+      setHasError(true);
+    });
   };
 
   return (
-    <Dialog open>
-      <DialogTitle>Ease Trip</DialogTitle>
-      <DialogContent>
-        <Grid rowSpacing={2}>
-          <EzForm
-            defaultValues={defaultValues}
-            schema={loginSchema}
-            onSubmit={handleSubmit}
-            onError={() => {
-              console.log('EzForm on form empty');
-            }}
-          >
-            <EzTextField fullWidth name="email" label="Email" />
-            <EzTextField
-              fullWidth
-              required
-              name="password"
-              label="Password"
-              type={showPassword ? 'text' : 'password'}
-              endAdornment={
-                <EzIconButton
-                  name={showPassword ? 'VisibilityOff' : 'Visibility'}
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                />
-              }
-            />
-            <EzCheckbox name="rememberMe" label="keep me signed in." />
-            <DialogActions>
-              <EzContainedButton type="submit">Sign In</EzContainedButton>
-            </DialogActions>
-          </EzForm>
-        </Grid>
-      </DialogContent>
-    </Dialog>
+    <Fragment>
+      <Dialog open>
+        <DialogTitle>Ease Trip</DialogTitle>
+        <DialogContent>
+          <Grid rowSpacing={2}>
+            <EzForm
+              defaultValues={defaultValues}
+              schema={loginSchema}
+              onSubmit={handleSubmit}
+              onError={() => {
+                console.log('EzForm on form empty');
+              }}
+            >
+              <EzTextField fullWidth name="email" label="Email" />
+              <EzTextField
+                fullWidth
+                required
+                name="password"
+                label="Password"
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <EzIconButton
+                    name={showPassword ? 'VisibilityOff' : 'Visibility'}
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  />
+                }
+              />
+              <EzCheckbox name="rememberMe" label="keep me signed in." />
+              <DialogActions>
+                <EzContainedButton type="submit">Sign In</EzContainedButton>
+              </DialogActions>
+            </EzForm>
+          </Grid>
+        </DialogContent>
+      </Dialog>
+      <Snackbar
+        open={hasError}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        autoHideDuration={5000}
+        TransitionComponent={(props) => <Slide {...props} direction="up" />}
+      >
+        <Alert severity="error" sx={{ width: '100%' }}>
+          login failure
+        </Alert>
+      </Snackbar>
+    </Fragment>
   );
 }
