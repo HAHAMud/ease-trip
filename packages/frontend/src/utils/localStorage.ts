782651164882b7b23cbd72976c0d8eefb1c20c3d
Elvina <qq861924@gmail.com>
2024-03-31 01:22:14 +0800

    fix: 🐛 set localstorage item error handler

--- packages/frontend/src/utils/localStorage.ts
+++ packages/frontend/src/utils/localStorage.ts
@@ -1,7 +1,11 @@
 
 export const setStorageItem = (key: string, value: object | string) => {
-  const isObject = value instanceof Object;
-  return localStorage.setItem(key, isObject ? JSON.stringify(value) : value);
+  try {
+    const isObject = value instanceof Object;
+    return localStorage.setItem(key, isObject ? JSON.stringify(value) : value);
+  } catch (error) {
+    console.error("Error setting item in localStorage:", error);
+  }
 }
 
 export const getStorageItem = (key: string) => {
