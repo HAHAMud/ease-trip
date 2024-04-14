16af5e2a5f62c8389baf145293cf65174a255c7e
lee926427 <40841221+lee926427@users.noreply.github.com>
2024-04-14 22:06:40 +0800

    fix: 🐛 remove variant (#84)

--- apps/frontend/src/modules/Auth/views/Register.tsx
+++ apps/frontend/src/modules/Auth/views/Register.tsx
@@ -19,7 +19,6 @@ import { register } from '@/api/auth';
 import { defaultRegisterValues, RegisterFormProps, registerSchema } from '@/modules/Auth/models';
 
 export default function RegisterForm() {
-  const fromRef = useRef<HTMLFormElement>(null);
   const [showPassword, setShowPassword] = useState(false);
 
   const toast = useToast();
