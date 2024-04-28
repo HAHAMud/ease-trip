89f13a3b575dce440cdca12d6ce439d2a61f13fd
recoil <leev126g8@gmail.com>
2024-04-29 05:21:19 +0800

    chore: add font family config

--- apps/frontend/src/constants/materialThemeConfig.ts
+++ apps/frontend/src/constants/materialThemeConfig.ts
@@ -1,5 +1,4 @@
-import { ThemeOptions, colors } from '@ease-trip/easy-ui';
-const { green, purple } = colors;
+import { ThemeOptions } from '@ease-trip/easy-ui';
 
 /**
  * @description theme configuration for material ui
@@ -10,5 +9,19 @@ export const materialThemeConfig: ThemeOptions = {
     primary: { main: '#48C4C3' },
     secondary: { main: '#D9D9D9' },
     info: { main: '#6E6556' },
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
   },
 };
