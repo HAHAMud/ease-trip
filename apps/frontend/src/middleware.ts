39abb10424974cefd9ae90f66eb12af19360e7e2
Willy <54195426+willy874@users.noreply.github.com>
2024-05-04 22:49:25 +0800

    EE-88: refactor: middleware (#99)

--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -8,15 +8,3 @@ const getResponse = cond(middlewares);
 export function middleware(request: NextRequest) {
   return getResponse(request) ?? NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
-  ],
-};
