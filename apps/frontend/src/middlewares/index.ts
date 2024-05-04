8f83f3c886ccddc838b6da154616ad0b280cb55a
willy <willy8742891@gmail.com>
2024-05-04 22:47:22 +0800

    EE-88: refactor: middleware

--- apps/frontend/src/middlewares/index.ts
+++ apps/frontend/src/middlewares/index.ts
@@ -1,4 +1,13 @@
 import { authorizationMiddleware } from './authorization';
 import { Middleware, middlewareController } from './utils';
 
-export const middlewares: Middleware[] = [authorizationMiddleware(), [() => true, () => middlewareController.next()]];
+export const middlewares: Middleware[] = [
+  [
+    (req) => new RegExp('/((?!api|_next/static|_next/image|favicon.ico).*)').test(req.url),
+    () => middlewareController.next(),
+  ],
+  [(req) => req.headers.has('next-router-prefetch'), () => middlewareController.next()],
+  [(req) => req.headers.get('purpose') === 'prefetch', () => middlewareController.next()],
+  authorizationMiddleware(),
+  [() => true, () => middlewareController.next()],
+];
